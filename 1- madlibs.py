04d19d99d24e3b602b93421d46dec7d2462f1be4
Armen Haddad <67371378+Armen-GH@users.noreply.github.com>
2021-01-28 12:44:34 -0800

    Add files via upload

---
+++ 1- madlibs.py
@@ -0,0 +1,16 @@
+adj1 = input("Adjective: ")
+nationality = input("Nationality: ")
+person = input("Person: ")
+noun = input("Noun1: ")
+adj2 = input("Adjective: ")
+adj3 = input("Adjective: ")
+adj4 = input("Adjective: ")
+pn = input("Plural Noun: ")
+
+madlib = f"Pizza was invented by a {adj1} {nationality} chef named {person}. \
+To make a pizza, you need to take a bunch of {noun}, \
+and make a thin, round {adj2} {noun}. \
+Then you cover it with {adj3} sauce, {adj4} cheese and fresh chopped {pn}."
+
+print(madlib)
+
